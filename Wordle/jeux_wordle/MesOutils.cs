af51ad54ccd61e63f247b5b7d48be3d3499987f4
Simon Archambeau <210303@site.asty.moulin.be>
2023-05-30 00:08:35 +0200

    ajout du jeux

---
+++ Wordle/jeux_wordle/MesOutils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeux_wordle
+{
+    class MesOutils
+    {
+        public static string ChoisirMotCache()
+        {
+            string[] mots = {"pomme", "drone", "ecran", "bonjour", "soleil", "vendre", "plaque","cadeau", "souris", "fleur", "avoir", "dormir","argent", "patate", "boiter", "avion", "chante", "chance","danse", "esprit", "forêt", "gouter", "joindre","maison", "nouvel", "parler", "quitter", "savoir"};
+            Random random = new Random();
+            int indexMot = random.Next(0, mots.Length);
+            return mots[indexMot];
+        }
+
+        public static bool motCorrect(string tentative, string motCache)
+        {
+            int lettresCorrectesBienPlacees = 0;
+            int lettresCorrectesMalPlacees = 0;
+
+            for (int i = 0; i < motCache.Length; i++)
+            {
+                if (tentative[i] == motCache[i])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(tentative[i]);
+                    lettresCorrectesBienPlacees++;
+                }
+                else if (motCache.Contains(tentative[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write(tentative[i]);
+                    lettresCorrectesMalPlacees++;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(tentative[i]);
+                }
+            }
+
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Lettres bien placées : {0}", lettresCorrectesBienPlacees);
+            Console.WriteLine("Lettres mal placées : {0}", lettresCorrectesMalPlacees);
+            Console.WriteLine("Lettres incorrectes : {0}", motCache.Length - lettresCorrectesBienPlacees - lettresCorrectesMalPlacees);
+
+            return lettresCorrectesBienPlacees == motCache.Length;
+        }
+    }
+}
---
+++ Wordle/jeux_wordle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeux_wordle
+{
+    class Program
+    {
+        static void Main()
+        {
+            string motCache = MesOutils.ChoisirMotCache();
+            int essaisRestants = 6;
+
+            Console.WriteLine("Bienvenue dans Wordle !");
+            Console.WriteLine("Le mot à deviner contient {0} lettres.", motCache.Length);
+            Console.WriteLine("Vous disposez de {0} essais.", essaisRestants);
+
+            while (essaisRestants > 0)
+            {
+                Console.Write("Entrez un mot : ");
+                string tentative = Console.ReadLine();
+
+                if (tentative.Length != motCache.Length)
+                {
+                    Console.WriteLine("Le mot doit contenir {0} lettres.", motCache.Length);
+                    continue;
+                }
+
+                bool MotCorrect = MesOutils.motCorrect(tentative, motCache);
+                if (MotCorrect)
+                {
+                    Console.WriteLine("Félicitations, vous avez trouvé le mot !");
+                    break;
+                }
+
+                essaisRestants--;
+                Console.WriteLine("Mauvaise réponse. Il vous reste {0} essais.", essaisRestants);
+
+                if (essaisRestants == 0)
+                {
+                    Console.WriteLine("Vous avez épuisé tous vos essais. Le mot était : {0}", motCache);
+                }
+            }
+
+            Console.WriteLine("Merci d'avoir joué à Wordle !");
+            Console.ReadLine(); //  ajout d'une pause pour empêcher la fermeture automatique du programme
+        }
+    }
+}
